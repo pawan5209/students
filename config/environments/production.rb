08ee8ce00590ae1d54cc72303d364136ed4574d7
pawan5209 <pawan.p@namespace.jp>
2024-09-10 11:43:18 +0545

    pointing to domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,8 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  config.hosts << "aadesh.info.np"
+
 
 # require 'net/ssh/proxy/command'
 
